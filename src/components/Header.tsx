544ddff0741f0b5817b9ce0822d87fbc5a29e2ce
Poorna Maheez Uppala <poornamaheez2000@gmail.com>
2025-09-19 23:08:37 +0530

    testing with the branch

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,6 +8,7 @@ const Header: React.FC = () => {
         Start<span className="highlight">IT</span>
       </div>
       <nav>
+        {/* just for a testing purpose */}
         <ul className="nav-links">
           <li><a href="#home" className="active">Home</a></li>
           <li><a href="#about">About</a></li>
